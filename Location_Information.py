237ed7e751ddca80be1a25ec9955c6152fdddf8d
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2024-04-16 15:34:03 +0300

    SecondCommit

---
+++ Location_Information.py
@@ -0,0 +1,17 @@
+#Define variables for city, country, and continent
+city = "Amman" #Here, we create a variable named city and assign it the name of the city where you live.
+
+country= "Jordan" #Similarly, we create a variable named country and assign it the name of the country where you live.
+
+continent = "Asia" #Finally, we create a variable named continent and assign it the name of the continent where you live.
+
+#Display the information
+print("City:", city) #This line prints out the text "City:" followed by the value stored in the variable 'city'.
+#The 'Print' function is used to display information to the user.
+#In this case, it prints the name of the city where you live.
+
+print("Country:", country) #This line prints out the text "Country:" followed by the value stored in the variable 'country'.
+#Similar to the previous line, it prints the name of the country where you live.
+
+print("Continent:", continent) #This line prints out the text "Continent:" followed by the value stored in the variable 'continent'.
+#Again, it prints the name of the continent where you live.
--- Mutliply_Age.py
+++ Mutliply_Age.py
@@ -1,13 +1,14 @@
 #Prompt the user to enter their age
 
 age = int(input("Enter your age: ")) #Here, it asks the user to input their age.
-#The input function takes whatever the user types in as a string.
-#So we use int to convert it to an integer which is a whole number.
+#The 'input' function takes whatever the user types in as a string.
+#So we use 'int' to convert it to an integer which is a whole number.
 
 #Multiply the age by 2
-result = age * 2 #We Multiply the age entered by the user by 2 using the * operator.
-#And story the result in the variable result.
+result = age * 2 #We Multiply the age entered by the user by 2 using the '*' operator.
+#And story the result in the variable 'result'.
 
 #Display the result
 print("Twice your age is:", result) #Finally, we print out the result to the user.
-#The print function is used to display information to the user.
+#The line prints out the text "Twice your age is:" followed by the value stored in the variable 'result'. 
+#The 'print' function is used to display information to the user.
