70a6413a368428eddb1bf5914eb9ab22fa91c015
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2024-04-16 17:29:09 +0300

    FirstCommit

---
+++ SemesterSchedule.py
@@ -0,0 +1,15 @@
+#Define variables for the starting and ending day of the semester
+start_day = "Thursday" #We create a variable named 'start_day' and assign it the starting day of the examination schedule.
+
+
+end_day = "Sunday" #Similarly, we create a variable named 'end_day' and assign it the ending of the examination schedule.
+
+#Display the semester schedule
+
+print("Semester Schedule:") #This line prints out the heading "Semester Schedule:" to indicate what the following information is about.
+
+print("Start Day:", start_day) #This line prints out the text "Start day:" followed by the value stored in the variable 'start_day'.
+#It displays the starting day of the semester schedule.
+
+print("End Day:", end_day) #This line prints out the text "End Day:" followed by the value stored in the variable 'end_day'.
+#It displays the ending day of the semester schedule.
