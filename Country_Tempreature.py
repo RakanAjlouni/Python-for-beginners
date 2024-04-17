e90e2df2f85afc525776c7f81b428abb5140bce4
Rakan Ajlouni <43892731+RakanAjlouni@users.noreply.github.com>
2024-04-17 06:25:25 +0300

    FirstCommit

---
+++ Country_Tempreature.py
@@ -0,0 +1,5 @@
+#Define a variable for the temperature of your country
+country_temperature = "25°C" #Here, we create a variable named 'country_temperature' to store the temperature of my country.
+
+#Display the temperature
+print("Temperature of Jordan Today:", country_temperature) #This line prints out the text "Temperature of My Country Today:" followed by the value stored in the 'country_temperature'.
